0b834a519151db84bbe42891a8dc8c57ed9622be
Dustin Nguyen <dustin7212@gmail.com>
2022-04-20 00:27:01 -0500

    Update timer to display in command line

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Input the number of seconds: ");
+        int input = keyboard.nextInt();
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int time = input;
+
+            public void run() {
+                System.out.println(time);
+                time--;
+
+                if (time < 0) {
+                    timer.cancel();
+                    System.out.println("Time's up!");
+                }
+            }
+        }, 0, 1000);
+    }
+
+}
